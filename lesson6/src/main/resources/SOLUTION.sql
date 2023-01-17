e2370121586a741865fa481b64e9c55c37cdff4d
Tanya Matseonzhek <matseonzhek@gmail.com>
2023-01-17 18:58:44 +0400

    stage2-module3-SQL-tasks DONE second attempt

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 select * from payment where type_id = (select id from paymenttype where NAME ='MONTHLY');
 select MARK  from mark where SUBJECT_ID = (SELECT ID FROM SUBJECT WHERE NAME = 'Art');
-SELECT STUDENT.NAME FROM STUDENT INNER JOIN PAYMENT on STUDENT.ID = PAYMENT.STUDENT_ID where PAYMENT.TYPE_ID = (select id from paymenttype where NAME ='WEEKLY');
+SELECT DISTINCT STUDENT.NAME FROM STUDENT INNER JOIN PAYMENT on STUDENT.ID = PAYMENT.STUDENT_ID where PAYMENT.TYPE_ID = (select id from paymenttype where NAME ='WEEKLY');
 SELECT STUDENT.NAME FROM STUDENT INNER JOIN MARK on STUDENT.ID = MARK .STUDENT_ID where MARK .SUBJECT_ID  = (select id from SUBJECT where NAME ='Math');
